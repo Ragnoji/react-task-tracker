a930904128fa7c8a33c2a43092d660e2044a251d
Ragnoji <dok.hok@mail.ru>
2023-03-06 16:07:42 +0300

    added Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line import/no-named-as-default
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div className="App">
-      <Header>
-        <p>Task Tracker</p>
-      </Header>
+      <Header />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Wrapper = styled.header`
+  display: flex;
+  background: cornflowerblue;
+`;
+
+const Footer = () => {
+  return <Wrapper>©️ 2023 Almaz Stepanov</Wrapper>;
+};
+
+export default Footer;

0dc4bef1c2e702334de8901f03c8a4b948d52b54
Ragnoji <dok.hok@mail.ru>
2023-03-06 15:54:20 +0300

    added wrapper for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
-const Header = styled.header`
+const Wrapper = styled.header`
   display: flex;
   position: sticky;
   justify-content: space-between;
   background: cornflowerblue;
 `;
+
+const Header = () => {
+  return <Wrapper>Text Header</Wrapper>;
+};
 
 export default Header;

cc879f96cfdaf574e27c02267e9b761734c0990a
Ragnoji <dok.hok@mail.ru>
2023-03-06 15:09:43 +0300

    added linters and solved offense

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Task Tracker
-        </p>
+        <p>Task Tracker</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'reset-css';
-import GlobalStyle from './globalStyle';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "reset-css";
+import GlobalStyle from "./globalStyle";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function

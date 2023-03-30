3ac377d0e34e1fab5377b6fd02be71890eeb4343
Ragnoji <dok.hok@mail.ru>
2023-03-30 20:26:55 +0300

    added BackToTop button in DefaultTemplate

--- src/components/templates/DefaultTemplate/DefaultTemplate.jsx
+++ src/components/templates/DefaultTemplate/DefaultTemplate.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Header from "../../organisms/Header/Header";
 import Footer from "../../organisms/Footer/Footer";
@@ -13,13 +14,50 @@ const MainContent = styled.main`
   display: flex;
   background: mintcream;
   justify-content: center;
+  flex-direction: column;
+`;
+
+const BackToTop = styled.button`
+  position: fixed;
+  top: 30px;
+  right: 20px;
+  font-size: 24px;
+  background: dimgray;
+  color: white;
+  cursor: pointer;
+  border-radius: 100px;
+  border: none;
+  box-shadow: 0 5px 10px #ccc;
+  z-index: 100;
 `;
 
 const DefaultTemplate = ({ children }) => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 0) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // for smoothly scrolling
+    });
+  };
+
   return (
     <Wrapper>
       <Header />
-      <MainContent>{children}</MainContent>
+      <MainContent>
+        {showButton && <BackToTop onClick={scrollToTop}>Scroll to top</BackToTop>}
+        {children}
+      </MainContent>
       <Footer />
     </Wrapper>
   );
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import TableRow from "../components/molecules/TableRow/TableRow";
 import Table from "../components/organisms/Table/Table";
+import DefaultTemplate from "../components/templates/DefaultTemplate/DefaultTemplate";
 
-const ProjectsIndex = () => {
+const ProjectsPage = () => {
   const [projects] = useState([
     {
       id: 1,
@@ -24,15 +25,62 @@ const ProjectsIndex = () => {
       title: "4 project",
       description: "bla bla",
     },
+    {
+      id: 5,
+      title: "5 project",
+      description: "bla bla",
+    },
+    {
+      id: 6,
+      title: "6 project",
+      description: "bla bla",
+    },
+    {
+      id: 7,
+      title: "7 project",
+      description: "bla bla",
+    },
+    {
+      id: 8,
+      title: "8 project",
+      description: "bla bla",
+    },
+    {
+      id: 9,
+      title: "9 project",
+      description: "bla bla",
+    },
+    {
+      id: 10,
+      title: "10 project",
+      description: "bla bla",
+    },
+    {
+      id: 11,
+      title: "11 project",
+      description: "bla bla",
+    },
+    {
+      id: 12,
+      title: "12 project",
+      description: "bla bla",
+    },
+    {
+      id: 13,
+      title: "13 project",
+      description: "bla bla",
+    },
   ]);
 
   return (
-    <Table title="Projects">
-      {projects?.map((val) => (
-        <TableRow key={val.id} title={val.title} description={val.description} colorFlag={val.id % 2 === 0 && true} />
-      ))}
-    </Table>
+    <DefaultTemplate>
+      <Table title="Projects">
+        {projects?.map((val) => (
+          <TableRow key={val.id} title={val.title} description={val.description} colorFlag={val.id % 2 === 0} />
+        ))}
+      </Table>
+    </DefaultTemplate>
   );
 };
 
-export default ProjectsIndex;
+export default ProjectsPage;
